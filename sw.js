4a393087f5e716b9e4f5b2aed39df3e845cc7291
Yann <yannick.avellana@gmail.com>
2026-05-13 10:10:45 +0200

    Fix PWA: chemins relatifs pour GitHub Pages

--- sw.js
+++ sw.js
@@ -1,24 +1,49 @@
-const CACHE = "lombra-v10";
-const URLS = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
+var CACHE = "lombra-v11";
+var URLS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./chapters.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(URLS).catch(function() {
+        // Si chapters.js ou icons manquent, on cache au moins le core
+        return c.addAll(["./", "./index.html", "./manifest.json"]);
+      });
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(ks) {
+      return Promise.all(
+        ks.filter(function(k) { return k !== CACHE; })
+          .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", function(e) {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (res.ok && e.request.method === "GET") {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match("/index.html")))
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        if (res.ok && e.request.method === "GET") {
+          var clone = res.clone();
+          caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+        }
+        return res;
+      }).catch(function() {
+        return caches.match("./index.html");
+      });
+    })
   );
 });
